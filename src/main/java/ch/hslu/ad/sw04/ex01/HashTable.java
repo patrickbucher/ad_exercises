f635af01d849f9ae6e7b700a7018e946730c2148
patrickbucher <patrick.bucher@stud.hslu.ch>
2017-03-20 10:42:27 +0100

    worked on AD exercise 4.2

--- src/main/java/ch/hslu/ad/sw04/ex01/HashTable.java
+++ src/main/java/ch/hslu/ad/sw04/ex01/HashTable.java
@@ -1,17 +1,39 @@
 package ch.hslu.ad.sw04.ex01;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class HashTable {
 
-	public static final int SIZE = 10;
+	public static final int SIZE = 12;
 
 	private Object entries[] = new Object[SIZE];
 
+	private int size = 0;
+
 	public boolean put(Object entry) {
 		int index = calculateIndex(entry);
-		if (entries[index] != null) {
+		int collisionDomain = index;
+
+		// look for next empty space
+		while (index < SIZE && entries[index] != null) {
+			if (entries[index].equals(entry)) {
+				// already contained: abort
+				return false;
+			}
+			if (calculateIndex(entries[index]) != collisionDomain) {
+				// end of chain reached: abort
+				return false;
+			}
+			index++;
+		}
+
+		if (index == SIZE) {
 			return false;
 		}
+
 		entries[index] = entry;
+		size++;
 		return true;
 	}
 
@@ -21,12 +43,49 @@ public class HashTable {
 			return false;
 		}
 		entries[index] = null;
+		size--;
 		return true;
 	}
 
 	public Object get(int hashCode) {
 		int index = calculateIndex(hashCode);
+		int collisionDomain = index;
+		
+		while (index < SIZE && entries[index] != null) {
+			if (entries[index].hashCode() == hashCode) {
+				// found element by hashCode: return it
+				return entries[index];
+			}
+			if (calculateIndex(entries[index]) != collisionDomain) {
+				// end of chain reached: abort
+				return false;
+			}
+			index++;
+		}
+		
+		if (index == SIZE) {
+			return false;
+		}
+		
 		return entries[index];
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public boolean isFull() {
+		return size == SIZE;
+	}
+	
+	public Collection<Object> getAllElements() {
+		Collection<Object> allElements = new ArrayList<>(getSize());
+		for (int i = 0; i < SIZE; i++) {
+			if (entries[i] != null) {
+				allElements.add(entries[i]);
+			}
+		}
+		return allElements;
 	}
 
 	private int calculateIndex(Object entry) {
--- src/test/java/ch/hslu/ad/sw04/ex01/HashTableTest.java
+++ src/test/java/ch/hslu/ad/sw04/ex01/HashTableTest.java
@@ -1,5 +1,7 @@
 package ch.hslu.ad.sw04.ex01;
 
+import java.util.Collection;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,4 +33,131 @@ public class HashTableTest {
 		Assert.assertEquals(dog, table.get(dog.hashCode()));
 		Assert.assertNull(table.get("Cat".hashCode()));
 	}
+
+	@Test
+	public void testSizeEmpty() {
+		HashTable table = new HashTable();
+		Assert.assertEquals(0, table.getSize());
+		Assert.assertFalse(table.isFull());
+	}
+
+	@Test
+	public void testSizeFull() {
+		HashTable table = new HashTable();
+		final int fill = HashTable.SIZE;
+		for (char c = 'a'; c < ('a' + fill); c++) {
+			table.put(String.valueOf(c));
+		}
+		Assert.assertEquals(fill, table.getSize());
+		Assert.assertTrue(table.isFull());
+	}
+
+	@Test
+	public void createIndexCollision() {
+		Character first = 'a';
+		Character second = 'a' + HashTable.SIZE;
+		int firstIndex = first.hashCode() % HashTable.SIZE;
+		int secondIndex = second.hashCode() % HashTable.SIZE;
+		Assert.assertEquals(firstIndex, secondIndex);
+	}
+
+	@Test
+	public void testPutCollidingEntries() {
+		HashTable table = new HashTable();
+		Character first = 'a';
+		Character second = 'a' + HashTable.SIZE;
+		Character last = 'c';
+		table.put(first);
+		table.put(last);
+		// now: [-][a][-][c][-]
+
+		Assert.assertTrue(table.put(second));
+		// now: [-][-][a][X][c][-]
+		Assert.assertEquals(3, table.getSize());
+
+		Character third = 'a' + HashTable.SIZE * 2;
+		// it's not allowed to insert it at the right of c!
+		Assert.assertFalse(table.put(third));
+		Assert.assertEquals(3, table.getSize());
+	}
+
+	@Test
+	public void testGetWithCollidingEntries() {
+		HashTable table = new HashTable();
+		Character first = 'a';
+		table.put(first);
+		Character last = 'd';
+		table.put(last);
+		Character second = 'a' + HashTable.SIZE;
+		table.put(second);
+		Character third = 'a' + HashTable.SIZE * 2;
+		table.put(third);
+		// now: [-][a][X][Y][d]
+		Assert.assertEquals(first, table.get(first.hashCode()));
+		Assert.assertEquals(second, table.get(second.hashCode()));
+		Assert.assertEquals(third, table.get(third.hashCode()));
+		Assert.assertEquals(last, table.get(last.hashCode()));
+	}
+
+	@Test
+	public void testPutFullTable() {
+		HashTable table = new HashTable();
+		char c = findEntryMappingToIndexZero();
+		while (!table.isFull()) {
+			table.put(c);
+			c += HashTable.SIZE; // same collision domain
+		}
+		Assert.assertEquals(HashTable.SIZE, table.getSize());
+		Assert.assertFalse(table.put(c));
+	}
+
+	@Test
+	public void testGetFullTable() {
+		HashTable table = new HashTable();
+		char c = findEntryMappingToIndexZero();
+		Character last = 0;
+		while (!table.isFull()) {
+			table.put(c);
+			last = c;
+			c += HashTable.SIZE; // same collision domain
+		}
+		Assert.assertEquals(last, table.get(last.hashCode()));
+	}
+
+	@Test
+	public void testGetAllElements() {
+		HashTable table = new HashTable();
+		table.put('a');
+		table.put('b');
+		table.put('c');
+		Collection<Object> allElements = table.getAllElements();
+		Assert.assertEquals(3, allElements.size());
+		Assert.assertTrue(allElements.contains('a'));
+		Assert.assertTrue(allElements.contains('b'));
+		Assert.assertTrue(allElements.contains('c'));
+	}
+
+	@Test
+	public void breakCollisionDomain() {
+		HashTable table = new HashTable();
+		Character first = 'a';
+		Character second = 'a' + HashTable.SIZE;
+		Character third = 'a' + HashTable.SIZE * 2;
+		table.put(first);
+		table.put(second);
+		table.put(third);
+		Assert.assertEquals(3, table.getSize());
+		Assert.assertEquals(third, table.get(third.hashCode()));
+		table.remove(second);
+		Assert.assertNotNull(table.get(third.hashCode())); // fails
+	}
+
+	private Character findEntryMappingToIndexZero() {
+		Character c = 'a';
+		while (c.hashCode() % HashTable.SIZE != 0) {
+			c++;
+		}
+		return c;
+	}
+
 }
